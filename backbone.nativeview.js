a5c6bc79721e61227fefe78062ab18b654e70752
Daniel Jackson <daniel@cloakedninjas.co.uk>
2017-07-26 11:48:19 +0100

    Handle blur + focus event handlers which do not bubble to Document

--- backbone.nativeview.js
+++ backbone.nativeview.js
@@ -116,6 +116,17 @@
         selector = null;
       }
 
+      // Given that `focus` and `blur` events do not bubble, do not delegate these events
+      if (['focus', 'blur'].indexOf(eventName) !== -1) {
+        var els = this.el.querySelectorAll(selector);
+        for (var i = 0, len = els.length; i < len; i++) {
+          var item = els[i];
+          elementAddEventListener.call(item, eventName, listener, false);
+          this._domEvents.push({el: item, eventName: eventName, handler: listener});
+        }
+        return listener;
+      }
+
       var root = this.el;
       var handler = selector ? function (e) {
         var node = e.target || e.srcElement;
@@ -128,7 +139,7 @@
       } : listener;
 
       elementAddEventListener.call(this.el, eventName, handler, false);
-      this._domEvents.push({eventName: eventName, handler: handler, listener: listener, selector: selector});
+      this._domEvents.push({el: this.el, eventName: eventName, handler: handler, listener: listener, selector: selector});
       return handler;
     },
 
@@ -152,7 +163,7 @@
 
           if (!match) continue;
 
-          elementRemoveEventListener.call(this.el, item.eventName, item.handler, false);
+          elementRemoveEventListener.call(item.el, item.eventName, item.handler, false);
           this._domEvents.splice(i, 1);
         }
       }
@@ -164,7 +175,7 @@
       if (this.el) {
         for (var i = 0, len = this._domEvents.length; i < len; i++) {
           var item = this._domEvents[i];
-          elementRemoveEventListener.call(this.el, item.eventName, item.handler, false);
+          elementRemoveEventListener.call(item.el, item.eventName, item.handler, false);
         };
         this._domEvents.length = 0;
       }
